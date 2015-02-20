629fcbf5ba1c3e544cbcd72b4c8d6a71ec772984
mccannecology <mccannmikejames@gmail.com>
2015-02-20 12:09:41 -0500

    new threshold for GLM P/A based on specificity=sensitivity

---
+++ code/absences - glm - optimal threshold.R
@@ -0,0 +1,66 @@
+library(PresenceAbsence)
+
+load("C:/Users/Mike/Desktop/Dropbox/absences/workspace - avg glm - opt_threshold.RData")
+
+#ls()
+#rm(list=objects(pattern="^formula"))
+#rm(list=objects(pattern="^all"))
+#rm(list=objects(pattern="^glm"))
+#rm(list=objects(pattern="^best"))
+
+#save.image("C:/Users/Mike/Desktop/Dropbox/absences/workspace - avg glm - opt_threshold.RData")
+
+#########################
+# Build new data frames #
+#########################
+data_LM_threshold <- data.frame(seq(1,nrow(temp_data_LM),1),temp_data_LM$LM,predict(avg_LM_trans, temp_data_LM, type='response'))
+colnames(data_LM_threshold) <- c("site","obsv","pred")
+
+data_SP_threshold <- data.frame(seq(1,nrow(temp_data_SP),1),temp_data_SP$SP,predict(avg_SP_trans, temp_data_SP, type='response'))
+colnames(data_SP_threshold) <- c("site","obsv","pred")
+
+data_W_threshold <- data.frame(seq(1,nrow(temp_data_W),1),temp_data_W$W,predict(avg_W_trans, temp_data_W, type='response'))
+colnames(data_W_threshold) <- c("site","obsv","pred")
+
+data_FPpres_threshold <- data.frame(seq(1,nrow(temp_data_FPpres),1),temp_data_FPpres$FPpres,predict(avg_FPpres_trans, temp_data_FPpres, type='response'))
+colnames(data_FPpres_threshold) <- c("site","obsv","pred")
+
+########################## 
+# Find optimal threshold # 
+##########################
+optimal.thresholds(data_LM_threshold) # optimal thresholds for all methods 
+LM_threh <- as.numeric(optimal.thresholds(data_LM_threshold,opt.methods=2)[2]) # threshold where sensitivity=specificity 
+error.threshold.plot(data_LM_threshold,main="LM")
+
+optimal.thresholds(data_SP_threshold)
+SP_threh <- as.numeric(optimal.thresholds(data_SP_threshold,opt.methods=2)[2]) # threshold where sensitivity=specificity 
+error.threshold.plot(data_SP_threshold,main="SP")
+
+optimal.thresholds(data_W_threshold)
+W_threh <- as.numeric(optimal.thresholds(data_W_threshold,opt.methods=2)[2]) # threshold where sensitivity=specificity 
+error.threshold.plot(data_W_threshold,main="W")
+
+optimal.thresholds(data_FPpres_threshold)
+FPpres_threh <- as.numeric(optimal.thresholds(data_FPpres_threshold,opt.methods=2)[2]) # threshold where sensitivity=specificity 
+error.threshold.plot(data_FPpres_threshold,main="FPpres")
+
+############################ 
+# Re-do confusion matrices #
+############################
+LM_ConMat <- cmx(data_LM_threshold, threshold=LM_threh)
+SP_ConMat <- cmx(data_SP_threshold, threshold=SP_threh)
+W_ConMat <- cmx(data_W_threshold, threshold=W_threh)
+FPpres_ConMat <- cmx(data_FPpres_threshold, threshold=FPpres_threh)
+
+###################
+# calculate Kappa #
+###################
+Kappa(LM_ConMat)
+Kappa(SP_ConMat)
+Kappa(W_ConMat)
+Kappa(FPpres_ConMat)
+
+##############
+# save stuff #
+##############
+save.image("C:/Users/Mike/Desktop/Dropbox/absences/workspace - avg glm - opt_threshold.RData")
--- code/absences - mapping - ggplot - model averaging.R
+++ code/absences - mapping - ggplot - model averaging.R
@@ -1,5 +1,7 @@
 # http://uchicagoconsulting.wordpress.com/tag/r-ggplot2-maps-visualization/
 
+load("C:/Users/Mike/Desktop/Dropbox/absences/workspace - avg glm - opt_threshold.RData")
+
 library(MuMIn)
 library(ggplot2)
 library(maps)
@@ -7,6 +9,7 @@ library(maptools) # import for gcDestination
 library(grid)
 library(gridExtra)
 library(mapproj)
+library(scales)
 
 #load us map data
 all_states <- map_data("state")
@@ -75,6 +78,11 @@ p <- p + geom_point( data=data_glm_map, aes(x=lon, y=lat), color="coral1")
 p <- p + theme_classic()
 p
 
+####################### 
+# Set up color scales # 
+#######################
+cols <- rev(brewer_pal(pal = "RdBu")(5))
+
 ##############################################
 # combined figure with maps of LM, SP, and W #
 ##############################################
@@ -83,8 +91,11 @@ map_LM_glm2 <- map_LM_glm2 + geom_polygon(data=states, aes(x=long, y=lat, group 
 map_LM_glm2 <- map_LM_glm2 + coord_map()
 map_LM_glm2 <- map_LM_glm2 + geom_point(data=data_glm_map, aes(x=lon, y=lat, shape=as.factor(LM_obsv)),colour="black",size=5.5) 
 map_LM_glm2 <- map_LM_glm2 + geom_point(data=data_glm_map, aes(x=lon, y=lat, colour=LM_pred, shape=as.factor(LM_obsv)), size=4) 
-map_LM_glm2 <- map_LM_glm2 + scale_colour_gradient2(limits=c(0,1),name="Predicted",low="blue", mid="white", high="red",midpoint=0.5)
-map_LM_glm2 <- map_LM_glm2 + scale_fill_gradient2(limits=c(0,1),name="Predicted",low="blue", mid="white", high="red",midpoint=0.5)
+map_LM_glm2 <- map_LM_glm2 + scale_colour_gradientn(colours=cols,
+                                                    name="Predicted",
+                                                    values=rescale(c(0,LM_threh/2,LM_threh,LM_threh*3/2,max(data_glm_map$LM_pred))),
+                                                    guide="colorbar"
+                                                    )
 map_LM_glm2 <- map_LM_glm2 + scale_shape_discrete(name="Observed",labels=c("absent","present"))
 map_LM_glm2 <- map_LM_glm2 + theme_classic(base_size=18)
 map_LM_glm2 <- map_LM_glm2 + theme(line = element_blank(),             # get rid of axes 
@@ -94,7 +105,7 @@ map_LM_glm2 <- map_LM_glm2 + theme(line = element_blank(),             # get rid
                                  axis.ticks.y = element_blank(), 
                                  axis.title = element_blank(),
                                  rect = element_blank()
-)
+                            )
 map_LM_glm2
 
 map_SP_glm2 <- ggplot()
@@ -102,8 +113,12 @@ map_SP_glm2 <- map_SP_glm2 + geom_polygon(data=states, aes(x=long, y=lat, group 
 map_SP_glm2 <- map_SP_glm2 + coord_map()
 map_SP_glm2 <- map_SP_glm2 + geom_point(data=data_glm_map, aes(x=lon, y=lat, shape=as.factor(SP_obsv)),colour="black",size=5.5) 
 map_SP_glm2 <- map_SP_glm2 + geom_point(data=data_glm_map, aes(x=lon, y=lat, colour=SP_pred, shape=as.factor(SP_obsv)), size=4) 
-map_SP_glm2 <- map_SP_glm2 + scale_colour_gradient2(limits=c(0,1),name="Predicted",low="blue", mid="white", high="red",midpoint=0.5)
-map_SP_glm2 <- map_SP_glm2 + scale_fill_gradient2(limits=c(0,1),name="Predicted",low="blue", mid="white", high="red",midpoint=0.5)
+map_SP_glm2 <- map_SP_glm2 + scale_colour_gradientn(colours=cols,
+                                                  name="Predicted",
+                                                  values=rescale(c(0,SP_threh/2,SP_threh,SP_threh*3/2,max(data_glm_map$SP_pred))),
+                                                  guide="colorbar",
+                                                  limits=c(0,max(data_glm_map$SP_pred))
+                  )
 map_SP_glm2 <- map_SP_glm2 + scale_shape_discrete(name="Observed",labels=c("absent","present"))
 map_SP_glm2 <- map_SP_glm2 + theme_classic(base_size=18)
 map_SP_glm2 <- map_SP_glm2 + theme(line = element_blank(),             # get rid of axes 
@@ -121,7 +136,12 @@ map_W_glm2 <- map_W_glm2 + geom_polygon(data=states, aes(x=long, y=lat, group = 
 map_W_glm2 <- map_W_glm2 + coord_map()
 map_W_glm2 <- map_W_glm2 + geom_point(data=data_glm_map, aes(x=lon, y=lat, shape=as.factor(W_obsv)),colour="black",size=5.5) 
 map_W_glm2 <- map_W_glm2 + geom_point(data=data_glm_map, aes(x=lon, y=lat, colour=W_pred, shape=as.factor(W_obsv)), size=4) 
-map_W_glm2 <- map_W_glm2 + scale_colour_gradient2(limits=c(0,1),name="Predicted",low="blue", mid="white", high="red",midpoint=0.5)
+map_W_glm2 <- map_W_glm2 + scale_colour_gradientn(colours=cols,
+                                                    name="Predicted",
+                                                    values=rescale(c(0,W_threh/2,W_threh,W_threh*3/2,max(data_glm_map$W_pred))),
+                                                    guide="colorbar",
+                                                    limits=c(0,max(data_glm_map$W_pred))
+)
 map_W_glm2 <- map_W_glm2 + scale_shape_discrete(name="Observed",labels=c("absent","present"))
 map_W_glm2 <- map_W_glm2 + theme_classic(base_size=18)
 map_W_glm2 <- map_W_glm2 + theme(line = element_blank(),             # get rid of axes 
@@ -141,7 +161,12 @@ map_FPpres_glm2 <- map_FPpres_glm2 + geom_polygon(data=states, aes(x=long, y=lat
 map_FPpres_glm2 <- map_FPpres_glm2 + coord_map()
 map_FPpres_glm2 <- map_FPpres_glm2 + geom_point(data=data_glm_map, aes(x=lon, y=lat, shape=as.factor(FPpres_obsv)),colour="black",size=5.5) 
 map_FPpres_glm2 <- map_FPpres_glm2 + geom_point(data=data_glm_map, aes(x=lon, y=lat, colour=FPpres_pred, shape=as.factor(FPpres_obsv)), size=4) 
-map_FPpres_glm2 <- map_FPpres_glm2 + scale_colour_gradient2(limits=c(0,1),name="Predicted",low="blue", mid="white", high="red",midpoint=0.5)
+map_FPpres_glm2 <- map_FPpres_glm2 + scale_colour_gradientn(colours=cols,
+                                                  name="Predicted",
+                                                  values=rescale(c(0,FPpres_threh/2,FPpres_threh,FPpres_threh*3/2,max(data_glm_map$FPpres_pred))),
+                                                  guide="colorbar",
+                                                  limits=c(0,max(data_glm_map$FPpres_pred))
+)
 map_FPpres_glm2 <- map_FPpres_glm2 + scale_shape_discrete(name="Observed",labels=c("absent","present"))
 map_FPpres_glm2 <- map_FPpres_glm2 + theme_classic(base_size=18)
 map_FPpres_glm2 <- map_FPpres_glm2 + scaleBar(lon = -72.5, lat = 41.,          # add a scale bar
@@ -267,7 +292,7 @@ map_FPpres_glm <- map_FPpres_glm + geom_polygon(data=states, aes(x=long, y=lat, 
 map_FPpres_glm <- map_FPpres_glm + coord_map()
 map_FPpres_glm <- map_FPpres_glm + geom_point(data=data_glm_map, aes(x=lon, y=lat, shape=as.factor(FPpres_obsv)),colour="black",size=5.5) 
 map_FPpres_glm <- map_FPpres_glm + geom_point(data=data_glm_map, aes(x=lon, y=lat, colour=SP_pred, shape=as.factor(FPpres_obsv)), size=4) 
-map_FPpres_glm <- map_FPpres_glm + scale_colour_gradient2(limits=c(0,1),name="Predicted",low="blue", mid="white", high="red",midpoint=0.5)
+map_FPpres_glm <- map_FPpres_glm + scale_colour_gradient2(limits=c(0,1),name="Predicted",low="navyblue", mid="white", high="red",midpoint=0.5)
 map_FPpres_glm <- map_FPpres_glm + geom_point(data=data_glm_map, aes(x=lon, y=lat, colour=FPpres_pred, shape=as.factor(FPpres_obsv)), size=4) 
 map_FPpres_glm <- map_FPpres_glm + scale_shape_discrete(name="Observed",labels=c("absent","present"))
 #map_FPpres_glm <- map_FPpres_glm + ggtitle("Floating plant presence") 
@@ -297,7 +322,7 @@ map_FPrich_glm <- map_FPrich_glm + geom_polygon(data=states, aes(x=long, y=lat, 
 map_FPrich_glm <- map_FPrich_glm + coord_map()
 map_FPrich_glm <- map_FPrich_glm + geom_point(data=data_glm_map, aes(x=lon, y=lat, shape=as.factor(FPrich_obsv)),colour="black",size=5.5) 
 map_FPrich_glm <- map_FPrich_glm + geom_point(data=data_glm_map, aes(x=lon, y=lat, colour=FPrich_pred, shape=as.factor(FPrich_obsv)), size=4) 
-map_FPrich_glm <- map_FPrich_glm + scale_colour_gradient2(limits=c(0,4),name="Predicted",low="blue", mid="white", high="red",midpoint=2)
+map_FPrich_glm <- map_FPrich_glm + scale_colour_gradient2(limits=c(0,4),name="Predicted",low="navyblue", mid="white", high="red",midpoint=2)
 map_FPrich_glm <- map_FPrich_glm + geom_point(data=data_glm_map, aes(x=lon, y=lat, colour=FPrich_pred, shape=as.factor(FPrich_obsv)), size=4) 
 map_FPrich_glm <- map_FPrich_glm + scale_shape_manual(name="Observed",values=c(16,17,18,15,25))
 #map_FPrich_glm <- map_FPrich_glm + ggtitle("Floating plant richness") 
@@ -328,7 +353,7 @@ map_FPrich_glm <- map_FPrich_glm + geom_polygon(data=states, aes(x=long, y=lat, 
 map_FPrich_glm <- map_FPrich_glm + coord_map()
 map_FPrich_glm <- map_FPrich_glm + geom_point(data=data_glm_map, aes(x=lon, y=lat, colour=FPrich_obsv), size=5.5) 
 map_FPrich_glm <- map_FPrich_glm + geom_point(data=data_glm_map, aes(x=lon, y=lat, colour=FPrich_pred), size=4) 
-map_FPrich_glm <- map_FPrich_glm + scale_colour_gradient2(limits=c(0,4),name="Richness",low="blue", mid="white", high="red",midpoint=2)
+map_FPrich_glm <- map_FPrich_glm + scale_colour_gradient2(limits=c(0,4),name="Richness",low="navyblue", mid="white", high="red",midpoint=2)
 map_FPrich_glm <- map_FPrich_glm + theme_classic(base_size=18)
 map_FPrich_glm <- map_FPrich_glm + scaleBar(lon = -72.5, lat = 41.,          # add a scale bar
                                             distanceLon = 25, 
@@ -356,7 +381,7 @@ map_FPrich_glm <- map_FPrich_glm + geom_polygon(data=states, aes(x=long, y=lat, 
 map_FPrich_glm <- map_FPrich_glm + coord_map()
 map_FPrich_glm <- map_FPrich_glm + geom_point(data=data_glm_map, aes(x=lon, y=lat, fill=FPrich_obsv), colour="black", pch=21, size=5.5) 
 map_FPrich_glm <- map_FPrich_glm + geom_point(data=data_glm_map, aes(x=lon, y=lat, fill=FPrich_pred), colour="black", pch=21, size=3.5) 
-map_FPrich_glm <- map_FPrich_glm + scale_fill_gradientn("Richness",colours=c("blue","white","red"))
+map_FPrich_glm <- map_FPrich_glm + scale_fill_gradientn("Richness",colours=c("navyblue","white","red"))
 map_FPrich_glm <- map_FPrich_glm + theme_classic(base_size=18)
 map_FPrich_glm <- map_FPrich_glm + scaleBar(lon = -72.5, lat = 41.,          # add a scale bar
                                             distanceLon = 25, 
